9f588acce99d5c734b63c3f3fc09cd54f36aef17
Rai Wirawan <raiisyouth@gmail.com>
2025-11-07 15:44:38 +0800

    feat: Implement RootLayout and enhance navigation with guest and protected routes

---
+++ app/_layout.tsx
@@ -0,0 +1,48 @@
+import {
+	Inter_400Regular,
+	Inter_700Bold,
+	useFonts,
+} from "@expo-google-fonts/inter";
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+function RootLayout() {
+	const colorScheme = useColorScheme();
+	const [fontsLoaded] = useFonts({
+		"Inter-Bold": Inter_700Bold,
+		"Inter-Regular": Inter_400Regular,
+	});
+
+	useEffect(() => {
+		if (fontsLoaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	return (
+		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen name="index" />
+			</Stack>
+		</ThemeProvider>
+	);
+}
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,72 +1,127 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import ChangePasswordScreen from "./screens/ChangePasswordScreen";
-import EditProfileScreen from "./screens/EditProfileScreen";
-import ForgotPasswordScreen from "./screens/ForgotPasswordScreen";
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import NotificationScreen from "./screens/NotificationScreen";
-import OnboardingScreen from "./screens/OnboardingScreen";
-import RegisterScreen from "./screens/RegisterScreen";
+import { AuthProvider } from "@/providers/AuthProvider";
+import GuestRoute from "@/routes/GuestRoute";
+import ProtectedRoute from "@/routes/ProtectedRoute";
+
+import type {
+	ChangePasswordScreenProps,
+	EditProfileScreenProps,
+	ForgotPasswordScreenProps,
+	HomeScreenProps,
+	LoginScreenProps,
+	NotificationScreenProps,
+	OnboardingScreenProps,
+	RegisterScreenProps,
+} from "@/types/navigation";
+
+import ChangePasswordScreen from "@/screens/ChangePasswordScreen";
+import EditProfileScreen from "@/screens/EditProfileScreen";
+import ForgotPasswordScreen from "@/screens/ForgotPasswordScreen";
+import HomeScreen from "@/screens/HomeScreen";
+import LoginScreen from "@/screens/LoginScreen";
+import NotificationScreen from "@/screens/NotificationScreen";
+import OnboardingScreen from "@/screens/OnboardingScreen";
+import RegisterScreen from "@/screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
-		<SafeAreaProvider>
-			<Stack.Navigator
-				screenOptions={{ headerTitleAlign: "center" }}
-				initialRouteName="Onboarding"
-			>
-				<Stack.Screen
-					name="Onboarding"
-					component={OnboardingScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="Register"
-					component={RegisterScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="Login"
-					component={LoginScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="ForgotPassword"
-					component={ForgotPasswordScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="Home"
-					component={HomeScreen}
-					options={{ headerShown: false }}
-				/>
-				<Stack.Screen
-					name="EditProfile"
-					component={EditProfileScreen}
-					options={{ title: "Edit Profile" }}
-				/>
-				<Stack.Screen
-					name="ChangePassword"
-					component={ChangePasswordScreen}
-					options={{ title: "Change Password" }}
-				/>
-				<Stack.Screen
-					name="Notification"
-					component={NotificationScreen}
-					options={{
-						title: "Notifications",
-						headerShown: false,
-						presentation: "transparentModal",
-						animation: "fade",
-						contentStyle: { backgroundColor: "transparent" },
+		<AuthProvider>
+			<SafeAreaProvider>
+				<Stack.Navigator
+					screenOptions={{
+						headerTitleAlign: "center",
+						animation: "fade_from_bottom",
+						animationDuration: 0,
 					}}
-				/>
-			</Stack.Navigator>
-		</SafeAreaProvider>
+					initialRouteName="Onboarding"
+				>
+					{/* GUEST SCREENS */}
+					<Stack.Screen name="Onboarding" options={{ headerShown: false }}>
+						{(props) => (
+							<GuestRoute>
+								<OnboardingScreen {...(props as OnboardingScreenProps)} />
+							</GuestRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen name="Login" options={{ headerShown: false }}>
+						{(props) => (
+							<GuestRoute>
+								<LoginScreen {...(props as LoginScreenProps)} />
+							</GuestRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen name="Register" options={{ headerShown: false }}>
+						{(props) => (
+							<GuestRoute>
+								<RegisterScreen {...(props as RegisterScreenProps)} />
+							</GuestRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen name="ForgotPassword" options={{ headerShown: false }}>
+						{(props) => (
+							<GuestRoute>
+								<ForgotPasswordScreen
+									{...(props as ForgotPasswordScreenProps)}
+								/>
+							</GuestRoute>
+						)}
+					</Stack.Screen>
+
+					{/* PROTECTED SCREENS */}
+					<Stack.Screen name="Home" options={{ headerShown: false }}>
+						{(props) => (
+							<ProtectedRoute>
+								<HomeScreen {...(props as HomeScreenProps)} />
+							</ProtectedRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen name="EditProfile" options={{ title: "Edit Profile" }}>
+						{(props) => (
+							<ProtectedRoute>
+								<EditProfileScreen {...(props as EditProfileScreenProps)} />
+							</ProtectedRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen
+						name="ChangePassword"
+						options={{ title: "Change Password" }}
+					>
+						{(props) => (
+							<ProtectedRoute>
+								<ChangePasswordScreen
+									{...(props as ChangePasswordScreenProps)}
+								/>
+							</ProtectedRoute>
+						)}
+					</Stack.Screen>
+
+					<Stack.Screen
+						name="Notification"
+						options={{
+							title: "Notifications",
+							headerShown: false,
+							presentation: "transparentModal",
+							animation: "fade",
+							contentStyle: { backgroundColor: "transparent" },
+						}}
+					>
+						{(props) => (
+							<ProtectedRoute>
+								<NotificationScreen {...(props as NotificationScreenProps)} />
+							</ProtectedRoute>
+						)}
+					</Stack.Screen>
+				</Stack.Navigator>
+			</SafeAreaProvider>
+		</AuthProvider>
 	);
 }
